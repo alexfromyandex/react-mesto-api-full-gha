3b906da2ef3be37e0257abbe550577f6fa3595fa
Aleksandr Smelov <smelovalex14@gmail.com>
2023-11-09 19:09:50 +0200

    cors changes

--- backend/app.js
+++ backend/app.js
@@ -5,43 +5,29 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { rateLimit } = require('express-rate-limit');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
 const { signinValidation, signupValidation } = require('./middlewares/validators');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('./middlewares/cors');
 
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, limit: 100 });
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect(('mongodb://127.0.0.1:27017/mestodb'), {
+mongoose.connect((process.env.MONGO_DB), {
   useNewUrlParser: true,
 });
 
+app.use(cors({ credentials: true, origin: 'https://alexsng.mesto.nomoredomainsmonster.ru' }));
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      const requestHeaders = req.headers['access-control-request-headers'];
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      return res.end();
-    }
-  }
-  return next();
-});
 
 app.post('/signin', signinValidation, login);
 app.post('/signup', signupValidation, createUser);
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,26 +1,26 @@
-const allowedCors = [
-  'http://alexsng.mesto.nomoredomainsmonster.ru',
-  'https://alexsng.mesto.nomoredomainsmonster.ru',
+// const allowedCors = [
+//   'http://alexsng.mesto.nomoredomainsmonster.ru',
+//   'https://alexsng.mesto.nomoredomainsmonster.ru',
 
-];
+// ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-module.exports = { allowedCors, DEFAULT_ALLOWED_METHODS };
-// module.exports = (req, res, next) => {
-//   const { origin } = req.headers;
-//   console.log(origin);
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     const { method } = req;
-//     const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//     const requestHeaders = req.headers['access-control-request-headers'];
+// module.exports = { allowedCors, DEFAULT_ALLOWED_METHODS };
+// // module.exports = (req, res, next) => {
+// //   const { origin } = req.headers;
+// //   console.log(origin);
+// //   if (allowedCors.includes(origin)) {
+// //     res.header('Access-Control-Allow-Origin', origin);
+// //     const { method } = req;
+// //     const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// //     const requestHeaders = req.headers['access-control-request-headers'];
 
-//     if (method === 'OPTIONS') {
-//       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//       res.header('Access-Control-Allow-Headers', requestHeaders);
-//       return res.end();
-//     }
-//   }
-//   return next();
-// };
+// //     if (method === 'OPTIONS') {
+// //       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+// //       res.header('Access-Control-Allow-Headers', requestHeaders);
+// //       return res.end();
+// //     }
+// //   }
+// //   return next();
+// // };
